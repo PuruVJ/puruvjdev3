c45dab90e9bb0b89e4cd7ce542ba7842ad5d30f5
Puru Vijay <47742487+PuruVJ@users.noreply.github.com>
2021-06-18 23:05:27 +0530

    Service Worker (#6)
    * Commit
    
    * Add rrevusion info
    
    * Stremline

---
+++ src/service-worker.ts
@@ -0,0 +1,34 @@
+import { build } from '$service-worker';
+
+import { precacheAndRoute } from 'workbox-precaching';
+import { registerRoute } from 'workbox-routing';
+import { ExpirationPlugin } from 'workbox-expiration';
+import { CacheFirst } from 'workbox-strategies';
+
+precacheAndRoute(build.map((url) => ({ url, revision: null })));
+
+registerRoute(
+  ({ request }) => request.destination === 'image',
+  new CacheFirst({
+    cacheName: 'images',
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  }),
+);
+
+registerRoute(
+  ({ url }) => url.toString().endsWith('.json'),
+  new CacheFirst({
+    cacheName: 'blog-json',
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  }),
+);
