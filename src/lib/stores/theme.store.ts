c5f40fa9b273d371e1741f6c75dfcbfa54667523
Puru Vijay <devpuruvj@gmail.com>
2021-06-20 12:13:51 +0530

    Fix favicon

--- src/lib/stores/theme.store.ts
+++ src/lib/stores/theme.store.ts
@@ -5,21 +5,15 @@ export type Theme = 'light' | 'midday' | 'dark' | 'radioactive';
 
 export const theme = writable<Theme>('light');
 
-let initVal = '';
+let initialized = false;
 theme.subscribe((val) => {
   if (!browser) return;
 
   // Don't do anything if its first value
-  if (!initVal) {
-    initVal = val;
-    return;
-  }
+  if (!initialized) return (initialized = true);
 
   const { dataset } = document.body;
-
   dataset.theme = val;
 
   localStorage.setItem('theme', val);
-
-  initVal = val;
 });
