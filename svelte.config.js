ffe534e37e73ae1b8e184c60bb76994aecf4e9de
Puru Vijay <devpuruvj@gmail.com>
2021-06-21 10:30:21 +0530

    use vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,12 @@
 // @ts-check
 import preprocess from 'svelte-preprocess';
-import staticAdapter from '@sveltejs/adapter-static';
+import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     target: '#svelte',
-    adapter: staticAdapter({
-      fallback: '/404.html',
-    }),
+    adapter: vercel(),
     prerender: {
       crawl: true,
       enabled: true,
